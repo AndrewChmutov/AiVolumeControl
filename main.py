a144212c1654b7f2b7529458eef9634687ceb73d
Andrew Chmutov <ulman.andre@gmail.com>
2023-05-01 12:22:22 +0200

    Volume gesture control under linux

--- main.py
+++ main.py
@@ -3,13 +3,14 @@ import time
 import numpy as np
 from hand_detection import HandDetector
 from volume_changer import VolumeChanger
+import math
 
 
 
 def main(volume_ch: VolumeChanger):
     # video object, webcam
     cap = cv2.VideoCapture(0)
-    draw = False
+    draw = True
     detector = HandDetector()
 
     previous_time = current_time = 0
@@ -21,7 +22,34 @@ def main(volume_ch: VolumeChanger):
         _, img = cap.read()
 
         processed_img = detector.find_hands(img, draw)
+        coords = detector.find_landmark_coords(img.shape)
+        coords = [(i[1], i[2]) for i in coords]
 
+        if coords:
+            p1 = coords[4]
+            p2 = coords[8]
+
+            dist = math.hypot(p1[0] - p2[0], p1[1] - p2[1])
+            vol = np.interp(dist, [35, 200], volume_ch.getRange())
+            volume_ch.setVolume(vol)
+            
+            if draw:
+                cv2.circle(processed_img, p1, 15, (255, 255, 255), cv2.FILLED)
+                cv2.circle(processed_img, p2, 15, (255, 255, 255), cv2.FILLED)
+
+                cv2.line(processed_img, p1, p2, (255, 255, 255), 3)
+
+                center = (
+                    p1[0] // 2 + p2[0] // 2,
+                    p1[1] // 2 + p2[1] // 2
+                )
+                print(center)
+                if dist < 35:
+                    cv2.circle(processed_img, center, 15, (0, 255, 0), cv2.FILLED)
+                else:
+                    cv2.circle(processed_img, center, 15, (0, 0, 255), cv2.FILLED)
+                    
+        
 
         # FPS
         if draw:
